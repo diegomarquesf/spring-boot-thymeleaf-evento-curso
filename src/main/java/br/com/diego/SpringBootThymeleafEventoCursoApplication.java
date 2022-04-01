c01434f55397309fbaf273d736773855f0313926
Diego Marques <fariia_di@hotmail.com>
2022-04-01 15:05:53 -0300

    criacao do projeto

---
+++ src/main/java/br/com/diego/SpringBootThymeleafEventoCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.diego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafEventoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafEventoCursoApplication.class, args);
+	}
+
+}
