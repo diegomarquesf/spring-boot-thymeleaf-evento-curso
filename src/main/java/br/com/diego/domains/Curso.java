fb585cf9c4442408c7e93b1e12aa70fdfb08582e
Diego Marques <fariia_di@hotmail.com>
2022-04-01 16:22:53 -0300

    configuracao do banco de dados MySQL

--- src/main/java/br/com/diego/domains/Curso.java
+++ src/main/java/br/com/diego/domains/Curso.java
@@ -1,12 +1,26 @@
 package br.com.diego.domains;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
-public class Curso {
+@Entity
+@Table(name = "tb_curso")
+public class Curso implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String nome;
 	private String local;
